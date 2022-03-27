964d1799aadb1763a74a6a720336f6052052ef94
mxt629 <xuantien.06021990@gmail.com>
2022-03-27 10:46:21 +0700

    Initial Commit

---
+++ SS12 Exe2 Count Vowels and Consonants/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SS12 Exe2 Count Vowels and Consonants
+//
+//  Created by Little Chick on 27/03/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
